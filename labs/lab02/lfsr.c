8313877ddba1e7c03bf39c8835819cf8a9ef0f88
chenkexu <muki974889407@outlook.com>
2023-04-10 16:40:56 +0800

    learn about Makefile and lfsr in lab02

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -5,6 +5,7 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t newbit = ((*reg >> 5) ^ (*reg >> 3) ^ (*reg >> 2) ^ *reg) & 1;
+    *reg = (*reg >> 1) | (newbit << 15);
 }
 
